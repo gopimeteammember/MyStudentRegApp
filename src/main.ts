95137c839471f3ebdb8353287795bd21c6330b5c
Santhosh Kumar Radhakrishnan <sandy27kumar@gmail.com>
2025-09-29 19:27:52 +0530

    InitailCommit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { provideHttpClient } from '@angular/common/http';
+import 'zone.js';
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    // ... other providers (like provideRouter, provideAnimations)
+    
+    provideHttpClient() // <-- 2. Add the provider function here
+  ]
+}).catch(err => console.error(err));  
