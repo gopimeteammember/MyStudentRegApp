88eb8df744a37dc1115fb2d40f851d821200cecc
Gopi <kkgopinth1@gmail.com>
2025-10-03 16:39:46 +0530

    Working Fine

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZonelessChangeDetection(),
+//     provideRouter(routes), provideClientHydration(withEventReplay())
+//   ]
+// };
+
+import { ApplicationConfig , provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+// 🛑 Import provideHttpClient and withFetch
+import { provideHttpClient, withFetch } from '@angular/common/http'; 
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    // 🛑 FIX: Add provideHttpClient(withFetch()) here
+    provideHttpClient(withFetch()) 
   ]
 };
